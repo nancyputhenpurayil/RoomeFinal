e857056360b2516967cf2f88c690582fc10a4a36
sydneyrichman <79024831+sydneyrichman@users.noreply.github.com>
2024-04-20 12:37:42 -0400

    more

--- roomebit24/src/pages/Home.js
+++ roomebit24/src/pages/Home.js
@@ -1,14 +1,17 @@
 import Nav from '../components/Nav'
+import { useState } from 'react'
 
 const Home = () => {
-
+    const [showModal, setShowModal] = useState(false)
+    
     const authToken = false
+    
     const handleClick = () => {
         console.log('clicked')
     }
 
     return (
-        <>
+        <div className = "overlay">
         <Nav authToken={authToken}/>
         <div className = "home">
             <h1>Swipe Right </h1>
@@ -18,7 +21,7 @@ const Home = () => {
                 
             </button>
         </div>
-        </>
+        </div>
     )
 }
 export default Home
--- roomebit24/src/pages/Onboarding.js
+++ roomebit24/src/pages/Onboarding.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 const Onboarding = () => {
     return (
         <div>Onboarding</div>
