51c4926e372ccfdb4081b80bd2a8bca97cb1e757
sydneyrichman <79024831+sydneyrichman@users.noreply.github.com>
2024-04-20 14:08:42 -0400

    new

--- roomebit24/src/pages/Home.js
+++ roomebit24/src/pages/Home.js
@@ -1,45 +1,33 @@
 import Nav from '../components/Nav'
-import AuthModal from "../components/AuthModal"
-import {useState} from 'react'
-import {useCookies} from "react-cookie"
+import AuthModal from '../components/AuthModal'
+import { useState } from 'react'
 
 const Home = () => {
     const [showModal, setShowModal] = useState(false)
-    const [isSignUp, setIsSignUp] = useState(true)
-    const [cookies, setCookie, removeCookie] = useCookies(['user'])
-    const authToken = cookies.AuthToken
-
+    
+    const authToken = false
+    
     const handleClick = () => {
-        if (authToken) {
-            removeCookie('UserId', cookies.UserId)
-            removeCookie('AuthToken', cookies.AuthToken)
-            window.location.reload()
-            return
-        }
+        console.log('clicked')
         setShowModal(true)
-        setIsSignUp(true)
     }
 
     return (
-        <div className="overlay">
-            <Nav
-                authToken={authToken}
-                minimal={false}
-                setShowModal={setShowModal}
-                showModal={showModal}
-                setIsSignUp={setIsSignUp}
-            />
-            <div className="home">
-                <h1 className="primary-title">Swipe Right®</h1>
-                <button className="primary-button" onClick={handleClick}>
-                    {authToken ? 'Signout' : 'Create Account'}
-                </button>
+        <div className = "overlay">
+        <Nav authToken={authToken} setShowModal={setShowModal}/>
+        <div className = "home">
+            <h1>Swipe Right </h1>
+            <button className = "primary-button" onClick={handleClick}>
+                {authToken ? 'Signout' : 'Create Account'}
 
+                
+            </button>
 
-                {showModal && (
-                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
-                )}
-            </div>
+            {showModal && (
+                <AuthModal setShowModal = {setShowModal}/>
+            )}
+
+        </div>
         </div>
     )
 }
