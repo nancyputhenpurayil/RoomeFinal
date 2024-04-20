eefeed21b30e1d89b89c32f5b27e791995613877
nancyputhenpurayil <nancybijo@gmail.com>
2024-04-20 13:59:39 -0400

    mit

--- roomebit24/src/App.js
+++ roomebit24/src/App.js
@@ -1,18 +1,24 @@
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
-import Onboarding from './pages/Onboarding'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import OnBoarding from './pages/Onboarding'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {useCookies} from 'react-cookie'
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path= "/" element= {<Home/>}/>
-        <Route path="/dashboard" element= {<Dashboard/>}/>
-        <Route path="/onboarding" element= {<Onboarding/>}/>
-      </Routes>
-    </BrowserRouter>
-  )
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
+
+    const authToken = cookies.AuthToken
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                {authToken && <Route path="/dashboard" element={<Dashboard/>}/>}
+                {authToken && <Route path="/onboarding" element={<OnBoarding/>}/>}
+
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
--- roomebit24/src/components/Nav.js
+++ roomebit24/src/components/Nav.js
@@ -1,15 +1,32 @@
 import logo from '../images/7081305.png'
 
-const Nav = ({authToken}) => {
 
+const Nav = ({ authToken, setShowModal, showModal, setIsSignUp }) => {
+    const handleClick = () => {
+      setShowModal(true);
+      setIsSignUp(false);
+    };
+  
     return (
-        <nav>
-            <div className="logo-container">
-                <img className="logo" src={logo}/>
-            </div>
-
-            {!authToken && <button className='nav-button'>Log in</button>}
-        </nav>
-    )
-}
-export default Nav
+      <nav>
+        <div className="logo-container">
+          <img
+            className="logo"
+            src={logo}
+            alt="logo"
+          />
+        </div>
+        {!authToken && !minimal && (
+          <button
+            className="nav-button"
+            onClick={handleClick}
+            disabled={showModal}
+          >
+            Log in
+          </button>
+        )}
+      </nav>
+    );
+  };
+  export default Nav;
+  
