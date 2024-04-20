133baa99549d7311fd9346aa3a7300b2e9325eca
sydneyrichman <79024831+sydneyrichman@users.noreply.github.com>
2024-04-20 13:59:20 -0400

    main

--- roomebit24/src/components/Nav.js
+++ roomebit24/src/components/Nav.js
@@ -1,14 +1,17 @@
 import logo from '../images/7081305.png'
 
-const Nav = ({authToken}) => {
+const Nav = ({ authToken, setShowModal }) => {
 
+    const handleClick = () => {
+        setShowModal(true)
+    }
     return (
         <nav>
             <div className="logo-container">
                 <img className="logo" src={logo}/>
             </div>
 
-            {!authToken && <button className='nav-button'>Log in</button>}
+            {!authToken && <button className='nav-button' onClick = {handleClick}>Log in</button>}
         </nav>
     )
 }
--- roomebit24/src/pages/Home.js
+++ roomebit24/src/pages/Home.js
@@ -1,33 +1,45 @@
 import Nav from '../components/Nav'
-import AuthModal from '../components/AuthModal'
-import { useState } from 'react'
+import AuthModal from "../components/AuthModal"
+import {useState} from 'react'
+import {useCookies} from "react-cookie"
 
 const Home = () => {
     const [showModal, setShowModal] = useState(false)
-    
-    const authToken = false
-    
+    const [isSignUp, setIsSignUp] = useState(true)
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
+    const authToken = cookies.AuthToken
+
     const handleClick = () => {
-        console.log('clicked')
+        if (authToken) {
+            removeCookie('UserId', cookies.UserId)
+            removeCookie('AuthToken', cookies.AuthToken)
+            window.location.reload()
+            return
+        }
         setShowModal(true)
+        setIsSignUp(true)
     }
 
     return (
-        <div className = "overlay">
-        <Nav authToken={authToken}/>
-        <div className = "home">
-            <h1>Swipe Right </h1>
-            <button className = "primary-button" onClick={handleClick}>
-                {authToken ? 'Signout' : 'Create Account'}
+        <div className="overlay">
+            <Nav
+                authToken={authToken}
+                minimal={false}
+                setShowModal={setShowModal}
+                showModal={showModal}
+                setIsSignUp={setIsSignUp}
+            />
+            <div className="home">
+                <h1 className="primary-title">Swipe Right®</h1>
+                <button className="primary-button" onClick={handleClick}>
+                    {authToken ? 'Signout' : 'Create Account'}
+                </button>
 
-                
-            </button>
 
-            {showModal && (
-                <AuthModal setShowModal = {setShowModal}/>
-            )}
-
-        </div>
+                {showModal && (
+                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
+                )}
+            </div>
         </div>
     )
 }
