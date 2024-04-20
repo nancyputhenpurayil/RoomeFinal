f7ef4a4c8e36351db5c315de79c8bf3da131dd47
nancyputhenpurayil <nancybijo@gmail.com>
2024-04-20 18:55:28 -0400

    Add white box when click log in and create acc

--- roomebit24/src/components/Nav.js
+++ roomebit24/src/components/Nav.js
@@ -1,6 +1,10 @@
 import logo from '../images/7081305.png'
 
-const Nav = ({authToken}) => {
+const Nav = ({authToken, setShowModal}) => {
+
+    const handleClick = () =>{
+        setShowModal(true)
+    }
 
     return (
         <nav>
@@ -8,7 +12,10 @@ const Nav = ({authToken}) => {
                 <img className="logo" src={logo}/>
             </div>
 
-            {!authToken && <button className='nav-button'>Log in</button>}
+            {!authToken && <button 
+            className="nav-button"
+            onClick={handleClick}
+            >Log in</button>}
         </nav>
     )
 }
--- roomebit24/src/pages/Home.js
+++ roomebit24/src/pages/Home.js
@@ -1,6 +1,6 @@
 import Nav from '../components/Nav'
 import AuthModal from '../components/AuthModal'
-import { useState } from 'react'
+import {useState} from 'react'
 
 const Home = () => {
     const [showModal, setShowModal] = useState(false)
@@ -16,15 +16,13 @@ const Home = () => {
         <div className = "overlay">
         <Nav authToken={authToken} setShowModal={setShowModal}/>
         <div className = "home">
-            <h1>Swipe Right </h1>
+            <h1>ROOME</h1>
             <button className = "primary-button" onClick={handleClick}>
-                {authToken ? 'Signout' : 'Create Account'}
-
-                
+                {authToken ? 'Signout' : 'Create Account'}       
             </button>
 
             {showModal && (
-                <AuthModal setShowModal = {setShowModal}/>
+                <AuthModal setShowModal={setShowModal}/>
             )}
 
         </div>
