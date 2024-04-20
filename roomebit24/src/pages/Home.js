2a9eb0e10f8e79a3b9ed4102e1270aa9ac04e63c
nancyputhenpurayil <nancybijo@gmail.com>
2024-04-20 10:10:33 -0400

    syd

--- roomebit24/src/pages/Home.js
+++ roomebit24/src/pages/Home.js
@@ -1,10 +1,22 @@
-const Home = () => {
-    console.log("Hello world")
-    return (
-        <div>
-            
+import Nav from '../components/Nav'
 
+const Home = () => {
+
+    const authToken = false
+    const handleClick = () => {
+        console.log('clicked')
+    }
+
+    return (
+        <>
+        <Nav/>
+        <div className = "home">
+            <h1>Swipe Right </h1>
+            <button className = "primary-button" onClick={handleClick}>
+                {authToken ? 'Signout' : 'Create Account'}
+            </button>
         </div>
+        </>
     )
 }
 export default Home
